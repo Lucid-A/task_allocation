410cc5263c90cdc06021c61e3363696e82ea8378
board_2 <nx@board_2>
2023-03-07 15:18:27 +0800

    format python codes and add comments

--- script/SA.py
+++ script/SA.py
@@ -1,6 +1,6 @@
-import numpy as np
 import math
 import random
+import numpy as np
 
 
 # 计算两点间距离
@@ -19,81 +19,86 @@ def distance_matrix(points):
 
 # SA分配算法
 class SA():
-    def __init__(self,strategy='MIN_DISTANCE'):
-        self.iters = 50 # 每轮温度迭代次数
-        self.temp = 100# 起始温度
-        self.temp_end = 10# 终止温度
-        self.decay = 0.99# 退火系数
-        self.strategy = strategy  # 最优策略：1距离最短，2时间最短
+    def __init__(self, strategy='MIN_DISTANCE'):
+        self.iters = 50     # 每轮温度迭代次数
+        self.decay = 0.99   # 退火系数
+        self.temp     = 100 # 起始温度
+        self.temp_end = 10  # 终止温度
+        
+        # 最优策略："MIN_TIME" or "MIN_DISTANCE"
+        self.strategy = strategy  
 
     #初始化解
-    def init_solution(self,point_index,robotnum,robot_status):
+    def init_solution(self, point_index, robotnum, robot_status):
         initial_solution = point_index[:]
         random.shuffle(initial_solution)
-        for i in range(0,robotnum-1):
-            initial_solution.insert(random.randint(0,len(initial_solution)), -1)#插入标记作，将路径分配给不同机器人
+        for i in range(0, robotnum-1):
+            # 插入标记，将路径分配给不同机器人
+            initial_solution.insert(random.randint(0, len(initial_solution)), -1)
         return initial_solution
 
     # 判断解的可行性
-    def verify_solution(self,robot_num,robot_status,task1num,task2num,solution):
-        routes= self.split_route(robot_num, solution)
+    def verify_solution(self, robot_num, robot_status, task1num, task2num, solution):
+        routes = self.split_route(robot_num, solution)
         for i in range(robot_num):
-            if robot_status[i][0]==0:
+            if 0 == robot_status[i][0]:
                 for pointindex in routes[i]:
-                    if pointindex<task1num:
+                    if pointindex < task1num:
                         return False
-            if robot_status[i][1]==0:
+            if 0 == robot_status[i][1]:
                 for pointindex in routes[i]:
-                    if pointindex >= task1num and pointindex<task1num+task2num:
+                    if pointindex >= task1num and pointindex < task1num + task2num:
                         return False
                 pass
         return True
 
     # 切分路径
-    def split_route(self,robotnum,solution):
+    def split_route(self, robotnum, solution):
         split_index = [i for i in range(len(solution)) if solution[i] == -1]
         split_index.insert(0, -1)
         split_index.append(len(solution))
-        routes=[]
+        
+        routes = []
         for i in range(0,robotnum):
-            route=solution[split_index[i]+1:split_index[i+1]]
+            route = solution[split_index[i]+1 : split_index[i+1]]
             routes.append(route)
         return routes
 
-    def calculate_cost(self,robot_num,robot_location,task_location,solution):
-        routes=self.split_route(robot_num,solution)
-        cost_list=[]
-        costs=0
+    def calculate_cost(self, robot_num, robot_location, task_location, solution):
+        costs = 0
+        cost_list = []
+        routes = self.split_route(robot_num, solution)
+
         # 总距离最短策略
-        if self.strategy=='MIN_DISTANCE':
-            for i in range(0,robot_num):
-                route=routes[i]
-                cost=0
-                if len(route)>0:
-                    cost+=distance(robot_location[i],task_location[route[0]])
-                    for j in range(0,len(route)-1):
-                        cost+=distance(task_location[route[j]],task_location[route[j+1]])
-                cost_list.append(cost)
-            costs=sum(cost_list)
-        # 总时间最短策略
-        elif self.strategy=='MIN_TIME':
-            cost_list=[]
+        if "MIN_DISTANCE" == self.strategy:
             for i in range(0, robot_num):
-                route = routes[i]
                 cost = 0
+                route = routes[i]
+                if len(route) > 0:
+                    cost += distance(robot_location[i], task_location[route[0]])
+                    for j in range(0,len(route)-1):
+                        cost += distance(task_location[route[j]], task_location[route[j+1]])
+                cost_list.append(cost)
+            costs = sum(cost_list)
+        # 总时间最短策略
+        elif "MIN_TIME" == self.strategy:
+            for i in range(0, robot_num):
+                cost = 0
+                route = routes[i]
                 if len(route) > 1:
                     cost += distance(robot_location[i], task_location[route[0]])
                     for j in range(0, len(route) - 1):
-                        cost += distance(task_location[route[j]],task_location[route[j+1]])
+                        cost += distance(task_location[route[j]], task_location[route[j+1]])
                 cost_list.append(cost)
-            costs=max(cost_list)+0.1*sum(cost_list)
+            costs = max(cost_list) + 0.1 * sum(cost_list)
+        
         return costs
 
     #生成新解
-    def new_solution(self,old):
+    def new_solution(self, old):
         p = random.random()
         # 交换法
-        if p<0.33:
+        if p < 0.33:
             while True:
                 pos1, pos2 = np.random.randint(0, len(old), size=2).tolist()
                 if pos1 != pos2:
@@ -101,17 +106,16 @@ class SA():
             new = old[:]
             swap = old[pos1], old[pos2]
             new[pos2], new[pos1] = swap
-
         # 移位法
-        elif p<0.66:
+        elif p < 0.66:
             while True:
-                pos= np.random.randint(0, len(old), size=3).tolist()
+                pos = np.random.randint(0, len(old), size=3).tolist()
                 if pos[0] != pos[1] and pos[1] != pos[2] and pos[0] != pos[2]:
                     break
             pos.sort()
             a=old[:pos[0]]
-            b=old[pos[0]:pos[1]]
-            c=old[pos[1]:pos[2]]
+            b=old[pos[0] : pos[1]]
+            c=old[pos[1] : pos[2]]
             d=old[pos[2]:]
             d.pop(0)
             new = a + c
@@ -121,52 +125,55 @@ class SA():
         else:
             while True:
                 pos1,pos2 = np.random.randint(0, len(old) - 1, size=2).tolist()
-                if pos2-pos1 >=1:
+                if pos2 - pos1 >=1:
                     break
             a=old[:pos1]
-            b=old[pos1:pos2+1][::-1]
+            b=old[pos1 : pos2+1][::-1]
             c=old[pos2+1:]
-            new=a+b+c
+            new = a + b + c
         return new
 
-    def solve(self,point_index,robot_location,task1_location,task2_location,robot_status):
-        D=distance_matrix(robot_location+task1_location+task2_location)
-        robot_num=len(robot_location)
+    def solve(self, point_index, robot_location, task1_location, task2_location, robot_status):
+        robot_num = len(robot_location)
+        D = distance_matrix(robot_location + task1_location + task2_location)
         init_solution = self.init_solution(point_index, robot_num, robot_status)
-        while self.verify_solution(robot_num,robot_status,len(task1_location),len(task2_location),init_solution)==False:
+        
+        while not self.verify_solution(robot_num, robot_status, len(task1_location), len(task2_location), init_solution):
             init_solution = self.init_solution(point_index, robot_num, robot_status)
-        init_cost=self.calculate_cost(robot_num,robot_location,task1_location+task2_location,init_solution)
-        print('initial_solution=', init_solution)
-        print('initial_cost=',init_cost)
-        best_solution=init_solution[:]
+        init_cost = self.calculate_cost(robot_num, robot_location, task1_location + task2_location, init_solution)
+        
+        print('initial_solution = ', init_solution)
+        print('initial_cost = ',init_cost)
+        
         best_cost = init_cost
+        best_solution = init_solution[:]
         current_solution = init_solution[:]
         current_cost = init_cost
 
         flag = 0
         ii = 0
-        while self.temp>self.temp_end:
+        while self.temp > self.temp_end:
             for i in range(self.iters):
-                new_solution= self.new_solution(current_solution)
+                new_solution = self.new_solution(current_solution)
                 while self.verify_solution(robot_num, robot_status, len(task1_location), len(task2_location),
                                            new_solution) == False:
                     new_solution = self.new_solution(current_solution)
-                new_cost=self.calculate_cost(robot_num,robot_location,task1_location+task2_location,new_solution)
-                if new_cost < current_cost or np.exp(
-                        -1 * np.abs(new_cost - current_cost) / self.temp) >= np.random.rand():
-                    current_solution = new_solution[:]
+                new_cost = self.calculate_cost(robot_num, robot_location, task1_location + task2_location, new_solution)
+                if new_cost < current_cost
+                   or np.exp(-1 * np.abs(new_cost - current_cost) / self.temp) >= np.random.rand():
                     current_cost = new_cost
+                    current_solution = new_solution[:]
                 if current_cost < best_cost:
                     flag = 0
-                    best_solution = current_solution[:]
                     best_cost = current_cost
+                    best_solution = current_solution[:]
 
             ii += 1
             flag += 1
 
             if flag == 5:
                 flag = 0
-                current_solution = best_solution
                 current_cost = best_cost
+                current_solution = best_solution
             self.temp = self.temp * self.decay
-        return best_solution,best_cost
+        return best_solution, best_cost
--- script/main.py
+++ script/main.py
@@ -2,32 +2,33 @@ import SA
 import random
 import matplotlib.pyplot as plt
 
+# args to be read from config file
+robot_num = 2
+robot_location = [[7,6], [0,0]] # The robot pose in map frame when calling task allocation 
+robot_status   = [[1,1], [1,1]] # robot_status[i][j] is 1 means robot[i+1] can carry out task[j+1]
+task1_location = [[3,3], [1,5], [7,9]] # Digital Recognize
+task2_location = [[0,1], [0,5], [7,8]] # Temperature Detection
 
-task1_location=[[3,3],[1,5],[7,9]]
-task2_location=[[0,1],[0,5],[7,8]]
-robot_num=2
-robot_location=[[7,6],[0,0]]# 机器人初始坐标
-robot_status=[[1,1],[1,1]]# 某处为1表示列表示第i个机器人可以去执行第j种任务
 
-def show(routes,robot_location):
-    task_lacation = task1_location+task2_location
-    x1,y1,x2,y2=[],[],[],[]
+def show(routes, robot_location):
+    task_lacation = task1_location + task2_location
+    x1, y1, x2, y2 = [], [], [], []
     for i in range(len(task1_location)):
         x1.append(task1_location[i][0])
         y1.append(task1_location[i][1])
     for i in range(len(task2_location)):
         x2.append(task2_location[i][0])
         y2.append(task2_location[i][1])
-    plt.scatter(x1,y1,c='red',s=50,marker='v', label='taskA')
-    plt.scatter(x2,y2,c='blue',s=40,marker='o', label='taskB')
+    plt.scatter(x1,y1, c='red',  s=50, marker='v', label='taskA')
+    plt.scatter(x2,y2, c='blue', s=40, marker='o', label='taskB')
     plt.legend()
-    plt.axis([min(x1+x2)-3, max(x1+x2)+3, min(y1+y2)-3,max(y1+y2)+3])
+    plt.axis([min(x1+x2)-3, max(x1+x2)+3, min(y1+y2)-3, max(y1+y2)+3])
 
     for i in range(len(routes)):
-        plt.scatter(robot_location[i][0],robot_location[i][1],s=30,marker='*',label='robot'+str(i+1))
-        route_x =[task_lacation[point][0] for point in routes[i]]
+        plt.scatter(robot_location[i][0], robot_location[i][1], s=30, marker='*', label='robot'+str(i+1))
+        route_x = [task_lacation[point][0] for point in routes[i]]
         route_y = [task_lacation[point][1] for point in routes[i]]
-        route_x.insert(0,robot_location[i][0])
+        route_x.insert(0, robot_location[i][0])
         route_y.insert(0, robot_location[i][1])
         plt.plot(route_x,route_y)
     plt.legend()
@@ -35,11 +36,15 @@ def show(routes,robot_location):
 
 
 if __name__ == '__main__':
-    points=robot_location+task1_location+task2_location
-    sa=SA.SA('MIN_TIME')
-    best_solution,best_cost=sa.solve([0,1,2,3,4,5],robot_location,task1_location,task2_location,robot_status)
-    routes=sa.split_route(robot_num,best_solution)
-    print('routes=',routes)
-    print('best_solution=',best_solution)
-    print('best_cost=',best_cost)
-    # show(routes,robot_location)
+    points = robot_location + task1_location + task2_location
+    sa = SA.SA('MIN_TIME')
+    best_solution, best_cost = sa.solve([0,1,2,3,4,5], robot_location, task1_location, task2_location, robot_status)
+    routes = sa.split_route(robot_num, best_solution)
+    
+    # for Cmd
+    print(f"routes = {routes}")
+    print(f"best_solution = {best_solution}")
+    print(f"best_cost = {best_cost}")
+    
+    ## for Graphic
+    # show(routes, robot_location)
