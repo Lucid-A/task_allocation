79cf130b41d01d821301b55aee35f030b1663e39
board1 <nx@board1>
2023-03-19 16:16:55 +0800

    To be tested: add task_allocation_cli

---
+++ src/task_allocation_cli.cpp
@@ -0,0 +1,12 @@
+#include "ros/ros.h"
+
+#include "task_allocation.hpp"
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, char* argv[], char* env[]) {
+    ros::init(argc, argv, "TAClient");
+    TAClient cli;
+    return 0;
+}
